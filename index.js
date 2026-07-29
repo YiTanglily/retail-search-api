bb5a74e34f4904894390151d2957942ec74cbfce
lili <tangyiamanda509@gmail.com>
2026-07-29 15:15:08 +0200

    sementic search

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from "express";
 import pg from "pg";
 import "dotenv/config";
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+async function embedding(text) {
+  const response = await openai.embeddings.create({
+    model: "text-embedding-3-small",
+    input: text,
+  });
+  return response.data[0].embedding;
+}
 
 const app = express();
 app.use(express.json());
@@ -25,9 +35,12 @@ app.get("/products/:id", async (req, res) => {
 
 app.post("/products", async (req, res) => {
   const { title, description, price, quantity } = req.body;
+  const embed = await embedding(description);
+  const vectorString = JSON.stringify(embed);
+  // pgvector doesn't accept arrays,so I convert "embed" to string style
   const result = await pool.query(
-    "INSERT INTO products (title, description, price, quantity) VALUES ($1, $2, $3, $4) RETURNING *",
-    [title, description, price, quantity],
+    "INSERT INTO products (title, description, price, quantity,embedding) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+    [title, description, price, quantity, embed],
   );
   res.send(result.rows[0]);
 });
